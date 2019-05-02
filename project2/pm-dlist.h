d2f751a76b62da55119e2602e1f08adc4a42e020
KpSimple <290784252@qq.com>
2019-05-02 16:30:30 +0800

    将私有属性放在.c文件

--- project2/pm-dlist.h
+++ project2/pm-dlist.h
@@ -3,22 +3,6 @@
 #include<glib-object.h>
 
 #define PM_TYPE_DLIST (pm_dlist_get_type())
-#define PM_DLIST_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE(obj,PM_TYPE_DLIST,PMDListPrivate))
-
-typedef struct _PMDListNode PMDListNode;
-struct _PMDListNode
-{
-	PMDListNode *prev;
-	PMDListNode *next;
-	void *data;
-};
-
-typedef struct _PMDListPrivate PMDListPrivate;
-struct _PMDListPrivate
-{
-	PMDListNode *head;
-	PMDListNode *tail;
-};
 
 typedef struct _PMDList PMDList;
 struct _PMDList
